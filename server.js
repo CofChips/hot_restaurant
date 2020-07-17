d08648fa8ecf488508b8a642534e44b3aaa16292
CofChips <qiwei.mod@gmail.com>
2020-07-17 10:54:10 -0700

    draft html files pending client side js

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"/assets/home.html"));
+})
+
+app.get("/tables",function(req,res){
+    res.sendFile(path.join(__dirname,"/assets/tables.html"));
+})
+
+app.get("/reserve",function(req,res){
+    res.sendFile(path.join(__dirname,"/assets/reserve.html"));
+})
+
 app.listen(PORT,function(req,res){
     console.log("App listening on PORT:"+PORT);
+
+
 })
